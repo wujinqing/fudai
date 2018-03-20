a64f49fa67f1fc4eb9670e7e5be3c9e7bbab4876
wujinqing <wujinqing@jetair.com.cn>
2018-03-20 18:00:12 +0800

    commit

---
+++ src/main/java/com/jin/fudai/util/FreeMarkerHelper.java
@@ -0,0 +1,64 @@
+package com.jin.fudai.util;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import com.jin.fudai.entity.SaleOrder;
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+
+
+public class FreeMarkerHelper {
+	private static final Configuration cfg;
+	private static final String TEMPLATE_NAME = "sale_order.ftl";
+	private static final String FTL_DIR = "/ftl";
+	private static final String TARGET_DIR = FreeMarkerHelper.class.getClassLoader().getResource("doc").getPath();
+
+	static
+	{
+		cfg = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
+		
+		try {
+			cfg.setClassForTemplateLoading(FreeMarkerHelper.class, FTL_DIR);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		cfg.setObjectWrapper(new DefaultObjectWrapper());
+		cfg.setDefaultEncoding("UTF-8");
+		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+	}
+	
+	public static Template getTemplate(String name)
+	{
+		try {
+			return cfg.getTemplate(name);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	private static void process(String targetFileName, SaleOrder dataModel)
+	{
+		try
+		{
+			Template template = getTemplate(TEMPLATE_NAME);
+			System.out.println(TARGET_DIR + targetFileName);
+			template.process(dataModel, new OutputStreamWriter(new FileOutputStream(TARGET_DIR + targetFileName)));
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		SaleOrder saleOrder = new SaleOrder();
+
+		process("zhansan.docx", saleOrder);
+
+		System.out.println("结束");
+	}
+}
